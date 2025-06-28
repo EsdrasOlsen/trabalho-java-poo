9eaa0e5e56dd7b0023c28ae09f0db32ccbaf3bfa
Esdras <esdraske@gmail.com>
2025-06-28 14:12:15 -0300

    Implementa Ex1 - classe Carro com constantes e impressão

--- src/Ex1.java
+++ src/Ex1.java
@@ -1,3 +1,28 @@
 public class Ex1 {
 
+        static class Carro{
+            int anoFabricacao;
+            int numeroPortas;
+
+            static final String MODELO = "Model T";
+            static final String COR = "Preto";
+
+            @Override
+            public String toString() {
+                return "Carro{" +
+                        "Ano da Fabricação: " + anoFabricacao +
+                        ", Número de Portas: " + numeroPortas +
+                        ", Modelo: " + MODELO +
+                        ", Cor: " + COR + '\"' +
+                        '}';
+            }
+        }
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+
+        carro.anoFabricacao = 1920;
+        carro.numeroPortas = 2;
+
+        System.out.println(carro);
+    }
 }
